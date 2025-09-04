c0ef6573f72d31390fc354b1139ea25e87db27e8
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-04 13:19:42 +0700

    Create gen_domain_ssl.sh

---
+++ support/gen_domain_ssl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#By Anggarda Saputra Wijaya
+#custom domain NGINX with SSL
+echo "   ==============================================   ";
+echo "              Generate Domain with SSL              ";
+echo "   ==============================================   ";
+echo -n " Custom DOMAIN : "
+read DOMAIN
+echo "                                                    ";
+sudo cp support/domain /etc/nginx/sites-available/$DOMAIN
+sed -i "s/domain/$DOMAIN/g" /etc/nginx/sites-available/$DOMAIN
+sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+sudo certbot --nginx -d $DOMAIN
