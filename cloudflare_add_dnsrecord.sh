3f62558c64b55d66391035667d16ea9333714436
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-04 15:00:16 +0700

    Create cloudflare_add_dnsrecord.sh

---
+++ cloudflare_add_dnsrecord.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "                                                    ";
+echo "                                                    ";
+echo -n "   Cloudflare Zone-ID      : "
+read ZONE_ID
+echo -n "   Cloudflare API-Key      : "
+read API_ID
+echo -n "   Add Domain DNS Record   : "
+read DOMAIN
+echo -n "   Pointing Public IP      : "
+read PUBLIC_IP
+echo "                                                    ";
+echo "                                                    ";
+sudo cp support/cloudflare_dns_record.sh add_dns.sh
+sed -i "s/zone_id/$ZONE_ID/g" add_dns.sh
+sed -i "s/api_id/$API_ID/g" add_dns.sh
+sed -i "s/domain/$DOMAIN/g" add_dns.sh
+sed -i "s/public_ip/$PUBLIC_IP/g" add_dns.sh
+chmod -R 777 add_dns.sh
+./add_dns.sh
+rm -rf add_dns.sh
