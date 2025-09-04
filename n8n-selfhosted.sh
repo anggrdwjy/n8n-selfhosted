f784d3f9e7b2dbe186a62170083fcb28b206268f
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-04 13:25:01 +0700

    Create n8n-selfhosted.sh

---
+++ n8n-selfhosted.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#By Anggarda Saputra Wijaya
+#Create Docker Container N8N Selfhosted Non-SSL
+echo "   ==============================================   ";
+echo "        Create Docker Container N8N Non-SSL         ";
+echo "   ==============================================   ";
+echo "                                                    ";
+echo -n " Create User for N8N Container      : "
+read USER
+echo -n " Create Password for N8N Container  : "
+read PASSWORD
+echo "                                                    ";
+echo "   ==============================================   ";
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+docker run -d --name n8n -p 5678:5678 -e N8N_BASIC_AUTH_ACTIVE=true -e N8N_BASIC_AUTH_USER=$USER -e N8N_BASIC_AUTH_PASSWORD=$PASSWORD -e N8N_SECURE_COOKIE=false -e N8N_PORT=5678 -e GENERIC_TIMEZONE=UTC+7 -v ~/.n8n:/home/node/.n8n n8nio/n8n
+docker ps -a
+sudo chown -R 1000:1000 ~/.n8n
+sudo chmod -R 755 ~/.n8n
+docker start n8n
+docker ps -a
+docker update --restart unless-stopped n8n
+echo "   ==============================================   ";
+echo "     Access Website [http://youripaddress:5678]     ";
+echo "   ==============================================   ";
