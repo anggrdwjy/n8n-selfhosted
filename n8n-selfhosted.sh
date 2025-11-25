3c5de7b93c1ad16d6249a56dd074b2ac129e9fbc
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-11-25 18:37:50 +0700

    Update n8n-selfhosted.sh

--- n8n-selfhosted.sh
+++ n8n-selfhosted.sh
@@ -15,9 +15,6 @@ echo "   ==============================================   ";
 echo "                                                    ";
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
 sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
