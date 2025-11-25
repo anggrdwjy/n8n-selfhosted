417cf0d54689a249d87e4d458ee0a024161d917d
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-11-25 18:41:05 +0700

    Update n8n-selfhosted-ssl.sh

--- n8n-selfhosted-ssl.sh
+++ n8n-selfhosted-ssl.sh
@@ -17,13 +17,10 @@ echo "   ==============================================   ";
 echo "                                                    ";
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
 sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
-sudo apt-get install docker.io docker-ce docker-ce-cli containerd.io -y
+sudo apt-get install docker.io -y
 docker run -d --name n8n -p 5678:5678 -e N8N_BASIC_AUTH_ACTIVE=true -e N8N_BASIC_AUTH_USER=$USER -e N8N_BASIC_AUTH_PASSWORD=$PASSWORD -e N8N_PORT=5678 -e N8N_HOST=$YOURDOMAIN -e WEBHOOK_URL=https://$YOURDOMAIN/ -e GENERIC_TIMEZONE=UTC+7 -v ~/.n8n:/home/node/.n8n n8nio/n8n
 docker ps -a
 sudo chown -R 1000:1000 ~/.n8n
