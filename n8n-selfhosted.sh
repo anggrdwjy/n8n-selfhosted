41a9f188b18919a77aeb292cb31a6e41b03a6c4d
Anggrdwjy <anggarda.wijaya410@gmail.com>
2026-01-04 00:14:01 +0700

    Update n8n-selfhosted.sh

--- n8n-selfhosted.sh
+++ n8n-selfhosted.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 #By Anggarda Saputra Wijaya
 #Create Docker Container N8N Selfhosted Non-SSL
-echo "                                                    ";
-echo "   ==============================================   ";
-echo "        Create Docker Container N8N Non-SSL         ";
-echo "   ==============================================   ";
-echo "                                                    ";
-echo -n "   Create User for N8N Container      : "
-read USER
-echo -n "   Create Password for N8N Container  : "
-read PASSWORD
-echo "                                                    ";
-echo "   ==============================================   ";
-echo "                                                    ";
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-sudo apt-get install docker.io -y
-docker run -d --name n8n -p 5678:5678 -e N8N_BASIC_AUTH_ACTIVE=true -e N8N_BASIC_AUTH_USER=$USER -e N8N_BASIC_AUTH_PASSWORD=$PASSWORD -e N8N_SECURE_COOKIE=false -e N8N_PORT=5678 -e GENERIC_TIMEZONE=UTC+7 -v ~/.n8n:/home/node/.n8n n8nio/n8n
+sudo apt-get update -y
+sudo apt-get install curl software-properties-common docker.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo apt-get update -y
+mkdir /home/portainer
+mkdir /home/n8n
+cp support/docker-portainer.yml /home/portainer/docker-compose.yml
+cp support/docker-n8n.yml /home/n8n/docker-compose.yml
+cd /home/portainer && docker-compose up -d
+cd /home/n8n && docker-compose up -d
 docker ps -a
-sudo chown -R 1000:1000 ~/.n8n
-sudo chmod -R 755 ~/.n8n
-docker start n8n
-docker ps -a
-docker update --restart unless-stopped n8n
-ufw allow 80
 ufw allow 22
-ufw allow 443
-ufw allow 5678
+ufw allow 19443
+ufw allow 15678
 ufw enable
 systemctl status ufw
-echo "   ==============================================   ";
-echo "     Access Website [http://youripaddress:5678]     ";
-echo "   ==============================================   ";
